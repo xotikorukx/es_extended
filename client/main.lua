c488021182623d2f233d39a29eff7cf9651ab233
xotikorukx <54357300+xotikorukx@users.noreply.github.com>
2020-02-23 22:14:15 -0600

    Revert "Implemented adding chat suggestions, closes #512"
    This reverts commit 8a832aee3bed36c734d1a34705289c9a7b2a9416.

--- client/main.lua
+++ client/main.lua
@@ -328,15 +328,6 @@ AddEventHandler('esx:createMissingPickups', function(missingPickups)
 	end
 end)
 
-RegisterNetEvent('esx:registerSuggestions')
-AddEventHandler('esx:registerSuggestions', function(registeredCommands)
-	for name,command in pairs(registeredCommands) do
-		if command.suggestion then
-			TriggerEvent('chat:addSuggestion', ('/%s'):format(name), command.suggestion.help, command.suggestion.arguments)
-		end
-	end
-end)
-
 RegisterNetEvent('esx:removePickup')
 AddEventHandler('esx:removePickup', function(id)
 	ESX.Game.DeleteObject(pickups[id].obj)

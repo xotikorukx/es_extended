608756e45abc5c4005d647c1999af38b1c89b378
xotikorukx <54357300+xotikorukx@users.noreply.github.com>
2020-02-23 22:14:17 -0600

    Revert "Override existing commands, and fixed rare bug with cb args all nil"
    This reverts commit 4b648a8bef4bd897891abd5508d5f4e5a1f63331.

--- server/functions.lua
+++ server/functions.lua
@@ -30,107 +30,103 @@ ESX.RegisterCommand = function(name, group, cb, allowConsole, suggestion)
 	end
 
 	if ESX.RegisteredCommands[name] then
-		print(('[es_extended] [^3WARNING^7] An command "%s" is already registered, overriding command'):format(name))
-	end
+		print(('[es_extended] [^3WARNING^7] An command "%s" is already registered'):format(name))
+	else
+		if suggestion then
+			if not suggestion.arguments then suggestion.arguments = {} end
+			if not suggestion.help then suggestion.help = '' end
+		end
 
-	if suggestion then
-		if not suggestion.arguments then suggestion.arguments = {} end
-		if not suggestion.help then suggestion.help = '' end
-	end
+		ESX.RegisteredCommands[name] = {group = group, cb = cb, allowConsole = allowConsole, suggestion = suggestion}
 
-	ESX.RegisteredCommands[name] = {group = group, cb = cb, allowConsole = allowConsole, suggestion = suggestion}
+		RegisterCommand(name, function(playerId, args, rawCommand)
+			local command = ESX.RegisteredCommands[name]
 
-	RegisterCommand(name, function(playerId, args, rawCommand)
-		local command = ESX.RegisteredCommands[name]
+			if not command.allowConsole and playerId == 0 then
+				print('[es_extended] [^3WARNING^7] That command can not be run from console')
+			else
+				local xPlayer, error = ESX.GetPlayerFromId(playerId), nil
 
-		if not command.allowConsole and playerId == 0 then
-			print('[es_extended] [^3WARNING^7] That command can not be run from console')
-		else
-			local xPlayer, error = ESX.GetPlayerFromId(playerId), nil
-
-			if command.suggestion then
-				if command.suggestion.validate then
-					if #args ~= #command.suggestion.arguments then
-						error = ('Argument count mismatch (passed %s, wanted %s)'):format(#args, #command.suggestion.arguments)
+				if command.suggestion then
+					if command.suggestion.validate then
+						if #args ~= #command.suggestion.arguments then
+							error = ('Argument count mismatch (passed %s, wanted %s)'):format(#args, #command.suggestion.arguments)
+						end
 					end
-				end
 
-				if not error and command.suggestion.arguments then
-					local newArgs = {}
+					if not error and command.suggestion.arguments then
+						local newArgs = {}
 
-					for k,v in ipairs(command.suggestion.arguments) do
-						if v.type then
-							if v.type == 'number' then
-								local newArg = tonumber(args[k])
+						for k,v in ipairs(command.suggestion.arguments) do
+							if v.type then
+								if v.type == 'number' then
+									local newArg = tonumber(args[k])
 
-								if newArg then
-									newArgs[v.name] = newArg
-								else
-									error = ('Argument #%s type mismatch (passed string, wanted number)'):format(k)
-								end
-							elseif v.type == 'player' or v.type == 'playerId' then
-								local targetPlayer = tonumber(args[k])
+									if newArg then
+										newArgs[v.name] = newArg
+									else
+										error = ('Argument #%s type mismatch (passed string, wanted number)'):format(k)
+									end
+								elseif v.type == 'player' then
+									local targetPlayer = tonumber(args[k])
 
-								if targetPlayer then
-									local xTargetPlayer = ESX.GetPlayerFromId(targetPlayer)
+									if targetPlayer then
+										local xTargetPlayer = ESX.GetPlayerFromId(targetPlayer)
 
-									if xTargetPlayer then
-										if v.type == 'player' then
+										if xTargetPlayer then
 											newArgs[v.name] = xTargetPlayer
 										else
-											newArgs[v.name] = targetPlayer
+											error = 'Player not online'
 										end
 									else
-										error = 'Player not online'
+										error = ('Argument #%s type mismatch (passed string, wanted number)'):format(k)
 									end
-								else
-									error = ('Argument #%s type mismatch (passed string, wanted number)'):format(k)
-								end
-							elseif v.type == 'string' then
-								newArgs[v.name] = args[k]
-							elseif v.type == 'item' then
-								if ESX.Items[args[k]] then
+								elseif v.type == 'string' then
 									newArgs[v.name] = args[k]
-								else
-									error = _U('invalid_item')
+								elseif v.type == 'item' then
+									if ESX.Items[args[k]] then
+										newArgs[v.name] = args[k]
+									else
+										error = _U('invalid_item')
+									end
+								elseif v.type == 'weapon' then
+									if ESX.GetWeapon(args[k]) then
+										newArgs[v.name] = string.upper(args[k])
+									else
+										error = 'Invalid weapon'
+									end
+								elseif v.type == 'any' then
+									newArgs[v.name] = args[k]
 								end
-							elseif v.type == 'weapon' then
-								if ESX.GetWeapon(args[k]) then
-									newArgs[v.name] = string.upper(args[k])
-								else
-									error = 'Invalid weapon'
-								end
-							elseif v.type == 'any' then
-								newArgs[v.name] = args[k]
 							end
+
+							if error then break end
 						end
 
-						if error then break end
+						args = newArgs
 					end
-
-					args = newArgs
 				end
-			end
 
-			if error then
-				if playerId == 0 then
-					print(('[es_extended] [^3WARNING^7] %s^7'):format(error))
-				else
-					xPlayer.triggerEvent('chat:addMessage', {args = {'^1SYSTEM', error}})
-				end
-			else
-				cb(xPlayer or false, args, function(msg)
+				if error then
 					if playerId == 0 then
-						print(('[es_extended] [^3WARNING^7] %s^7'):format(msg))
+						print(('[es_extended] [^3WARNING^7] %s^7'):format(error))
 					else
-						xPlayer.triggerEvent('chat:addMessage', {args = {'^1SYSTEM', msg}})
+						xPlayer.triggerEvent('chat:addMessage', {args = {'^1SYSTEM', error}})
 					end
-				end)
+				else
+					cb(xPlayer, args, function(msg)
+						if playerId == 0 then
+							print(('[es_extended] [^3WARNING^7] %s^7'):format(msg))
+						else
+							xPlayer.triggerEvent('chat:addMessage', {args = {'^1SYSTEM', msg}})
+						end
+					end)
+				end
 			end
-		end
-	end, true)
+		end, true)
 
-	ExecuteCommand(('add_ace group.%s command.%s allow'):format(group, name))
+		ExecuteCommand(('add_ace group.%s command.%s allow'):format(group, name))
+	end
 end
 
 ESX.ClearTimeout = function(id)
